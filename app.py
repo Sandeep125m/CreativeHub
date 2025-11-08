924ec5d16c6c6872550b7978baea760bd3a17744
Sandeep125m <sandeepmourya125@gmail.com>
2025-11-08 22:13:19 +0530

    favicon changes

--- app.py
+++ app.py
@@ -165,6 +165,10 @@ def check_and_expire_credits(user):
 def home():
     return render_template('home.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='favicon.ico'))
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
